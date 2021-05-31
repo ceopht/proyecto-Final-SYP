35e3936c16b744d4e6058db489eb7041adcef391
Ronald Benítez <ronald362001@gmail.com>
2021-05-31 13:19:51 -0600

    CRUD para clientes

---
+++ Database/clientesCRUD.php
@@ -0,0 +1,71 @@
+<?php 
+include ("connection.php");
+
+
+
+function mostrarDatosC(){
+    try {
+        //Conección y ejecución del query
+        $sql = "SELECT * FROM clientes";
+        $con=connect();
+        $resultado=mysqli_query($con,$sql);
+        $con=null;
+        return $resultado;
+    } catch (Exception $e) {
+        die(e->getMessage());
+    }
+    
+}
+
+function insertarCliente($nombre,$NRC){
+    try {
+        $con=connect();
+        $sql = "INSERT INTO clientes(nombre,NRC) VALUES('$nombre','$NRC')";
+        $resultado=mysqli_query($con,$sql);
+        $con=null;
+    } catch (Exception $e) {
+        die(e->getMessage());
+    }
+     
+}
+
+function editarCliente($nombre,$NRC,$id){
+    try {
+        $con=connect();
+        $sql = "UPDATE clientes SET nombre='$nombre', NRC='$NRC' WHERE idCliente='$id'";
+        $resultado=mysqli_query($con,$sql);
+        $con=null;
+    } catch (Exception $e) {
+        die(e->getMessage());
+    }
+}
+
+function borrarCliente($id){
+    try {
+        $con=connect();
+        $sql = "DELETE FROM clientes WHERE idCliente='$id'";
+        $resultado=mysqli_query($con,$sql);
+        $con=null;
+    } catch (Exception $e) {
+        die(e->getMessage());
+    }
+}
+
+function buscarCliente($valor,$opcion){
+    /*Se podra buscar el dato deseado
+    Las opciones son:
+    idCliente, nombre, NRC
+    */
+    try {
+        //Conección y ejecución del query
+        $sql = "SELECT * FROM clientes WHERE $opcion='$valor'";
+        $con=connect();
+        $resultado=mysqli_query($con,$sql);
+        $con=null;
+        return $resultado;
+    } catch (Exception $e) {
+        die(e->getMessage());
+    }
+}
+    
+?>
