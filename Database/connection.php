f002d3752c2e8ee98e7a3bda1a34972dc0dc7ef8
Ronald-Benitez <58644435+Ronald-Benitez@users.noreply.github.com>
2021-05-31 19:55:21 -0600

    Cambio en la conexión a una base de datos remota

--- Database/connection.php
+++ Database/connection.php
@@ -3,10 +3,10 @@
 function connect(){
 
     $conn = mysqli_connect(
-        'localhost',
-        'root',
-        '',
-        'records'
+        'bobe4yqqdi5erwefqczh-mysql.services.clever-cloud.com',
+        'udmu6yjnoih4eqeh',
+        'dxYM9CnWef6c2rAbIswX',
+        'bobe4yqqdi5erwefqczh'
       ) or die(mysqli_erro($mysqli));
     return $conn;
 }
