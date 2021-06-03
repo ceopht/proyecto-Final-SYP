1a2777d2efe845ce9dfee44cc575cb2ac781ccfc
Katherine <katherineamaya2200@gmail.com>
2021-06-02 23:20:25 -0600

    Agregando ventas por victoria y katherine

---
+++ Ventas/ventas.php
@@ -0,0 +1,88 @@
+<?php 
+    $fcha = date("Y-m-d");
+    include_once ('../Database/clientesCRUD.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Registro de ventas</title>
+</head>
+<body>
+    <div class="d-flex justify-content-center m-4">
+        <div class="col-md-4">
+        
+            <h3 class="text-center m-4">Registro de ventas</h3>
+
+            <form action="guardarVenta.php" method="post">
+
+                <div>
+                    <select class="form-select" aria-label="Default select example" id="tipo" name="tipo" onclick="generarCampos()">
+                        <option value="0" selected>Selecione tipo de venta</option>
+                        <option value="1">Consumidor final</option>
+                        <option value="2">Contribuyente</option>
+                    </select>
+                </div>
+
+                <div id="campos" style="display:none">
+
+                    <div class="m-4 fecha">
+                        <label class="form-label m-2">Fecha</label>
+                        <input type="date" name="fecha" class="validar m-2 form-control" value="<?php echo $fcha;?>">
+                    </div>
+
+                    <div class="m-4 factura">
+                        <label class="form-label m-2">Número de factura</label>
+                        <input type="number" name="factura" class="validar m-2 form-control">
+                    </div>
+
+                    <div class="m-4 total">
+                        <label class="form-label m-2">Total</label>
+                        <input type="number" name="total" class="validar1 m-2 form-control">
+                    </div>
+
+                    <div class="m-4 nombre">
+                        <label class="form-label m-2">Nombre del cliente</label>
+                        <select class="form-select" aria-label="Default select example" id="tipo" name="idCliente">
+                            <?php 
+                                $datos = mostrarDatosC();
+                                while($row = mysqli_fetch_assoc($datos)) {
+                                    $nombre = $row['nombre'];
+                                    $idCliente = $row['idCliente'];
+                                    echo "<option value='$idCliente'>$nombre</option>";
+                                }
+                            ?>
+                        </select>
+                        
+                    </div>
+
+                    <div class="m-4 venta">
+                        <label class="form-label m-2">Venta</label>
+                        <input type="number" name="venta" class="validar2 m-2 form-control">
+                    </div>
+
+                    <div class="m-4 IVA">
+                        <label class="form-label m-2">IVA</label>
+                        <input type="number" name="IVA" class="validar2 m-2 form-control">
+                    </div>
+                    
+                    <div class="text-center">
+                        <input type="submit" value="Registrar compra" class="btn btn-primary">
+                    </div>
+                </div>
+            </form>
+
+        </div>
+        
+    </div>
+    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="funcionesV.js"></script>
+</body>
+</html>
+
+<?php include_once ('funcionesJS.php');
