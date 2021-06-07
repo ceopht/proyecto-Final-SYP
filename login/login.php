64ddbc91d05406a91ab665b0d88782fbe8f3e6bf
Moises Beltran <moisesbeltran8930@gmail.com>
2021-06-06 21:46:03 -0600

    Interfaz de login

---
+++ login/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container mt-5" >
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card p-5 text-white" id="continerLogin">
+                <h5 class="card-title text-center">Login</h5>
+                <form>
+                    <div class="form-label-group">
+                        <label for="inputEmail">Nombre de usuario</label>
+                        <input type="text" id="usuario" class="form-control" placeholder="Usuario">
+                    </div>
+                    <br>
+                    <div class="form-label-group">
+                        <label for="inputPassword">Contraseña</label>
+                        <input type="password" id="contraseña" class="form-control" placeholder="*********">
+                    </div>
+                    <br>
+                    <button class="btn btn-outline-light" type="submit">Ingresar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+    <script src="validar_espacios.js"></script>
+</body>
+
+</html>
