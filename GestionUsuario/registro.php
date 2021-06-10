f1d8a9e21bee8ca811c5e33a19335fbb549e63ae
Gabriela67 <victoriavelasquez001212@gmail.com>
2021-06-09 22:39:40 -0600

    agregando archivo de registro

---
+++ GestionUsuario/registro.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+  <title>Document</title>
+</head>
+<body>
+<?php
+
+include_once ('../Database/usersCRUD.php');
+
+insertarUsuario($_POST['email'],$_POST['cargo'],$_POST['password']);
+
+    $_SESSION['message'] = 'usuario registrada';
+    $_SESSION['message_type'] = 'success';
+
+    echo '<div class= "alert alert-primary" >
+      agregado con exito
+      </div> ';
+
+  //header('Location: agregar.php');
+
+
+
+  // $datos = mostrarDatosU();
+
+//while($row = mysqli_fetch_assoc($datos))
+ //{
+   // echo "<pre>";
+  // print_r($row);
+  ///  echo "</pre>";
+//}
+?>
+<a href="formulario.php" class="btn btn-primary">REGRESAR</a>
+  
+</body>
+</html>
+
+
+
